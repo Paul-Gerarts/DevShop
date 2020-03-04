0a96fa6ec331bda8a555ca4b0604a51dfb651c07
lens <lens.huygh@gmail.com>
2020-03-04 15:32:21 +0100

    add H2 db dependency add application.properties: name, port, db-properties add controller method returning JSON String with title

--- src/main/java/be/syntra/devshop/DevshopBack/controller/TstRestController.java
+++ src/main/java/be/syntra/devshop/DevshopBack/controller/TstRestController.java
@@ -1,7 +1,16 @@
 package be.syntra.devshop.DevshopBack.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/devshop")
 public class TstRestController {
+
+    @GetMapping(value = "/test", produces ="application/JSON")
+    public ResponseEntity<String> displayTstString(){
+        return ResponseEntity.ok().body("DevShop");
+    }
 }
