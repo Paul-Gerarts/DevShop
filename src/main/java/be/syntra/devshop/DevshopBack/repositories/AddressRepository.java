7b09f296089744bca12bec46c90bacaf5e589dad
Fonthom <thomasf0n7a1n3@gmail.com>
2020-03-09 20:17:56 +0100

    Dev-007 annotated repository interfaces

--- src/main/java/be/syntra/devshop/DevshopBack/repositories/AddressRepository.java
+++ src/main/java/be/syntra/devshop/DevshopBack/repositories/AddressRepository.java
@@ -2,9 +2,11 @@ package be.syntra.devshop.DevshopBack.repositories;
 
 import be.syntra.devshop.DevshopBack.entities.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
     List<Address> findAll();
 }
--- src/main/java/be/syntra/devshop/DevshopBack/repositories/CartRepository.java
+++ src/main/java/be/syntra/devshop/DevshopBack/repositories/CartRepository.java
@@ -2,9 +2,11 @@ package be.syntra.devshop.DevshopBack.repositories;
 
 import be.syntra.devshop.DevshopBack.entities.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
     List<Cart> findAll();
 }
