ff5c6d982347042bb1804f2d498e57ac0637ff4e
Paul Gerarts <paul.gerarts@juvo.be>
2020-03-29 00:38:02 +0100

    Fixed nullpointer in CartServiceTest

--- src/main/java/be/syntra/devshop/DevshopBack/services/CartServiceImpl.java
+++ src/main/java/be/syntra/devshop/DevshopBack/services/CartServiceImpl.java
@@ -1,11 +1,13 @@
 package be.syntra.devshop.DevshopBack.services;
 
 
+import be.syntra.devshop.DevshopBack.entities.User;
 import be.syntra.devshop.DevshopBack.models.CartDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import static be.syntra.devshop.DevshopBack.services.utilities.CartMapperUtility.convertToCart;
+import static be.syntra.devshop.DevshopBack.services.utilities.UserMapperUtility.convertToUserDto;
 
 
 @Service
@@ -20,7 +22,9 @@ public class CartServiceImpl implements CartService {
 
     @Override
     public CartDto saveFinalizedCart(CartDto cartDto, Long userId) {
-        userService.getUserById(userId).getArchivedCarts().add(convertToCart(cartDto));
+        User user = userService.getUserById(userId);
+        user.getArchivedCarts().add(convertToCart(cartDto));
+        userService.save(convertToUserDto(user));
         return cartDto;
     }
 
--- src/test/java/be/syntra/devshop/DevshopBack/services/CartServiceTest.java
+++ src/test/java/be/syntra/devshop/DevshopBack/services/CartServiceTest.java
@@ -1,8 +1,8 @@
 package be.syntra.devshop.DevshopBack.services;
 
+import be.syntra.devshop.DevshopBack.entities.User;
 import be.syntra.devshop.DevshopBack.models.CartDto;
 import be.syntra.devshop.DevshopBack.repositories.UserRepository;
-import be.syntra.devshop.DevshopBack.services.utilities.CartMapperUtility;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -10,14 +10,16 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import static be.syntra.devshop.DevshopBack.testutilities.CartUtils.createCartDto;
+import static be.syntra.devshop.DevshopBack.testutilities.UserUtils.createUser;
+import static java.util.Optional.ofNullable;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 public class CartServiceTest {
+
     @Mock
-    private CartMapperUtility mapperUtility;
+    private UserServiceImpl userService;
 
     @Mock
     private UserRepository userRepository;
@@ -34,7 +36,11 @@ public class CartServiceTest {
     void saveFinalizedCartTest() {
         // given
         CartDto dummyDto = createCartDto();
+        User dummyUser = createUser();
         Long userId = 1L;
+        when(userService.getUserById(userId)).thenReturn(dummyUser);
+        when(userRepository.findById(userId)).thenReturn(ofNullable(dummyUser));
+        when(userRepository.save(dummyUser)).thenReturn(dummyUser);
         // when
         CartDto resultCartDto = cartService.saveFinalizedCart(dummyDto, userId);
         // then
@@ -44,6 +50,8 @@ public class CartServiceTest {
         assertEquals(dummyDto.getProducts().get(1).getName(), resultCartDto.getProducts().get(1).getName());
         assertEquals(dummyDto.getProducts().get(1).getPrice(), resultCartDto.getProducts().get(1).getPrice());
         verify(userRepository, times(1)).save(any());
+        verify(userService, times(1)).getUserById(userId);
+        verify(userRepository, times(1)).findById(userId);
     }
 
 }
